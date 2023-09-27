16a80b16aef4725e2188f67a57c28aa11eae3356
harshi9959 <harshinibollineni9@gmail.com>
2023-09-27 19:35:46 +0530

    finish

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -62,6 +62,7 @@ user['name']=user.email
 
         return user;
       },
+      
     }),
   ],
   pages: {
@@ -73,7 +74,23 @@ user['name']=user.email
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
   },
-  secret: process.env.NEXTAUTH_SECRET
-
+  secret: process.env.NEXTAUTH_SECRET,
+callbacks: {
+  async jwt({ token, account }) {
+    // Persist the OAuth access_token to the token right after signin
+    if (account) {
+      token.accessToken = account.access_token
+    }
+    return token
+  },
+  async redirect({ url, baseUrl }) {
+    return baseUrl
+  },
+  async session({ session, token, user }:any) {
+    // Send properties to the client, like an access_token from a provider.
+    session.accessToken = token.accessToken
+    return session
+  }
+}
 });
 
