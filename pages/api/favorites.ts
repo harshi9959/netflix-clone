379dfd6416ca9fd9b73621d0e1bdc3ad5c5676d4
harshi9959 <harshinibollineni9@gmail.com>
2023-09-28 18:57:39 +0530

    finsih

--- pages/api/favorites.ts
+++ pages/api/favorites.ts
@@ -5,7 +5,7 @@ import serverAuth from "@/lib/serverAuth";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
-      return res.status(405).json({place: "favourest api"});
+      return res.status(405).end();
     }
 
     try {
