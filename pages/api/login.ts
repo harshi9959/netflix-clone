54339d1367a0940f60f07671133e68748ca92ae6
harshi9959 <harshinibollineni9@gmail.com>
2023-09-29 11:48:26 +0530

    finsih

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -3,57 +3,36 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import prismadb from '@/lib/prismadb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    console.log(req, "req")
-    if (
-         req.method === "POST") {
-            try {
-    
-                // if (req.method !== 'POST') {
-                //   return res.status(405).end();
-                // }
-                // console.log(req, "req")
-                // const { email, password } = req.body;
-            
-                // const existingUser = await prismadb.user.findUnique({
-                //   where: {
-                //     email
-                //   }
-                // })
-                // if(!existingUser){
-                const {email,password} = req.body;
-                console.log(email, password)
-                 
-                // const user = await prismadb.user.findMany(
-            // )
-            // console.log(user, "user")
+    if (req.method === "POST") {
+        try {
+            const { email, password } = req.body;
             const user = await prismadb.user.findUnique({
                 where: {
                     email
                 }
             })
-            
-            const result = await bcrypt.compare(password,user.hashedPassword)
-            console.log(result, "result")
-            if (!result){
+
+            const result = await bcrypt.compare(password, user.hashedPassword)
+            if (!result) {
                 return (
-                    res.status(200).json({error:"wrong password"})
-                       )
+                    res.status(200).json({ error: "wrong password" })
+                )
             }
-            
-            
-               
-                return res.status(200).json({user:user})
-            
-            
-            
-                   
-              } catch (error) {
-                return res.status(400).json({ error: `Something went wrong: ${error}` });
-              }
-         }else{
-            return res.status(405).end();
-         }
-       
-  
+
+
+
+            return res.status(200).json({ user: user })
+
+
+
+
+        } catch (error) {
+            return res.status(400).json({ error: `Something went wrong: ${error}` });
+        }
+    } else {
+        return res.status(405).end();
+    }
+
+
 
 }
