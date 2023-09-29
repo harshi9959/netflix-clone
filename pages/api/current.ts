ebbdd0f5c9410e24f9ad503dbec29a9ba869f0d6
harshi9959 <harshinibollineni9@gmail.com>
2023-09-29 11:59:38 +0530

    finsih

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -2,16 +2,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 import serverAuth from "@/lib/serverAuth";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'GET') {
-      return res.status(405).end();
-    }
-
+    if (req.method !== 'GET') 
     try {
       const { currentUser } = await serverAuth(req);
-      
-    return res.status(200).json(currentUser);
+      return res.status(200).json(currentUser);
   } catch (error) {
     console.log(error);
-    return res.status(500).end();
+    return res.status(405).end();
   }
 }
